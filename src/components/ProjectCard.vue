0d1a579e9780a7284b1314e0ffa29479e4b0c32a
Sofia-Citrini <sofiacitrini@gmail.com>
2023-02-16 17:42:27 +0100

    created projectCard component

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="card" style="width: 18rem; height: 20rem;">
+        <img :src="url + 'storage/' + project.image" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">{{ project.title }}</h5>
+            <p class="card-text">{{ project.description }}</p>
+            <div class="badge text-bg-primary my-3">{{ project.type?.name }}</div>
+            <div v-if="project.technologies" v-for="tech in project.technologies" class="my-3 badge rounded-pill text-bg-danger">
+                {{ tech.name  }}
+            </div>
+            <a href="" class="my-3">{{ project.link_github }}</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        project: {
+            type:Object
+        }
+    },
+    data() {
+        return {
+            url:'http://127.0.0.1:8000/'
+        }
+    },
+        
+}
+</script>
+
+<style scoped lang="scss">
+.card{
+    overflow-y: auto;
+
+    img {
+        height: 8rem;
+        object-fit: cover;
+    }
+}
+</style>
--- src/pages/Projects/index.vue
+++ src/pages/Projects/index.vue
@@ -1,14 +1,20 @@
 <template>
-    <div>
-        projects
+    <h1 class="my-4">Projects</h1>
+
+    <div class="row g-4">
+        <div class="col-3" v-for="(project, i) in projects" :key="i">
+            <ProjectCard :project="project"></ProjectCard>
+        </div>
     </div>
 </template>
 
 <script>
 import axios from 'axios';
+import ProjectCard from '../../components/ProjectCard.vue'
 
 export default {
     name: 'ProjectsIndex',
+    components:{ProjectCard},
     data() {
         return {
             url:'http://127.0.0.1:8000/',
