56fcf3e65993f078c65150f06b75210f2215195a
Sofia-Citrini <sofiacitrini@gmail.com>
2023-02-20 18:37:31 +0100

    created loader component

---
+++ src/components/TheLoader.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="row g-5">
+        <div class="col-3" v-for="card in 8">
+            <div class="card" aria-hidden="true">
+                <div class="card-body">
+                    <h5 class="card-title placeholder-glow">
+                        <span class="placeholder col-6"></span>
+                    </h5>
+                    <p class="card-text placeholder-glow">
+                        <span class="placeholder col-7"></span>
+                        <span class="placeholder col-4"></span>
+                        <span class="placeholder col-4"></span>
+                        <span class="placeholder col-6"></span>
+                        <span class="placeholder col-8"></span>
+                    </p>
+                    <a href="#" tabindex="-1" class="btn btn-primary disabled placeholder col-6"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script></script>
+
+<style scoped lang="scss">
+.card{
+    height: 20rem;
+}
+</style>
--- src/pages/Projects/index.vue
+++ src/pages/Projects/index.vue
@@ -1,6 +1,8 @@
 <template>
     <h1 class="my-4">Projects</h1>
 
+    <TheLoader v-if="this.load"></TheLoader>
+
     <div class="row g-4">
         <div class="col-6 col-md-4 col-lg-3" v-for="(project, i) in projects" :key="i">
             <ProjectCard :project="project"></ProjectCard>
@@ -11,23 +13,29 @@
 <script>
 import axios from 'axios';
 import ProjectCard from '../../components/ProjectCard.vue';
+import TheLoader from '../../components/TheLoader.vue';
 import { store } from '../../store';
 
 export default {
     name: 'ProjectsIndex',
-    components:{ProjectCard},
+    components: { ProjectCard, TheLoader },
     data() {
         return {
             store,
-            projects: []
+            projects: [],
+            load: false
         }
     },
     methods: {
-        fetchProjects (){
+        fetchProjects() {
+            this.load = true;
+
             axios.get(store.url + 'api/projects')
                 .then((resp) => {
                     // console.log(resp.data);
                     this.projects = resp.data;
+
+                    this.load = false;
                 })
         }
     },
@@ -36,3 +44,4 @@ export default {
     },
 }
 </script>
+
